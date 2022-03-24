3791a4122730684f197e542b60c81c309727005e
Alessandro Zanin <56005440+wingo21@users.noreply.github.com>
2022-03-24 12:52:03 -0400

    now users can register their car (brand and model) when creating their account. Will show car brand and model in settings activity

--- app/src/main/java/com/example/internshipthesis/SettingsActivity.java
+++ app/src/main/java/com/example/internshipthesis/SettingsActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Objects;
@@ -17,11 +21,11 @@ import java.util.Objects;
 public class SettingsActivity extends AppCompatActivity {
 
     Button editSettings;
-    TextView yourEmail, emailShower;
+    TextView yourEmail, emailShower, userprofile;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth fAuth = FirebaseAuth.getInstance();
-    String username = Objects.requireNonNull(fAuth.getCurrentUser()).getDisplayName();
-    String email = fAuth.getCurrentUser().getEmail();
+    String username = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
+    String email = Objects.requireNonNull(fAuth.getCurrentUser()).getEmail();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +40,23 @@ public class SettingsActivity extends AppCompatActivity {
         editSettings = findViewById(R.id.edit_settings_button);
         yourEmail = findViewById(R.id.your_email);
         emailShower = findViewById(R.id.email_shower);
+        userprofile = findViewById(R.id.userprofile);
+
         emailShower.setText(email);
         editSettings.setOnClickListener(view -> openEditSettingsActivity());
+
+        db.collection("users")
+                .document(username)
+                .get().addOnCompleteListener(task -> {
+
+            if (task.isSuccessful()) {
+
+                String x = task.getResult().getString("name");
+                userprofile.setText(String.format("Ciao %s! Qui puoi visualizzare e modificare informazioni riguardanti il tuo profilo", x));
+
+            }
+        });
+
     }
 
     private void openEditSettingsActivity() {
--- app/src/main/java/com/example/internshipthesis/SignupActivity.java
+++ app/src/main/java/com/example/internshipthesis/SignupActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -15,6 +16,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Patterns;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -29,6 +34,9 @@ public class SignupActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth fAuth;
     String userID;
+    String brand;
+    private AutoCompleteTextView autoCompleteTextViewCarBrand;
+    private AutoCompleteTextView autoCompleteTextViewCarModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +54,132 @@ public class SignupActivity extends AppCompatActivity {
         inputEmail = findViewById(R.id.inputEmail);
         inputPassword = findViewById(R.id.inputPassword);
         confirmPassword = findViewById(R.id.confirmPassword);
+        autoCompleteTextViewCarBrand = findViewById(R.id.autoCompleteCarBrand);
+        autoCompleteTextViewCarModel = findViewById(R.id.autoCompleteCarModel);
         fAuth = FirebaseAuth.getInstance();
+
+        final String[] carBrands = new String[]{
+                "Lamborghini",
+                "Bugatti",
+                "Porsche",
+                "Bentley",
+                "Ferrari",
+                "Mercedes",
+        };
+
+        final String[] carModels = new String[]{
+                "Choose a car brand first",
+        };
+
+        final String[] lamborghiniModels = new String[]{
+                "Aventador",
+                "Urus",
+                "Sian",
+                "Huracàn",
+
+        };
+
+        final String[] bugattiModels = new String[]{
+                "Centodieci",
+                "Chiron",
+                "Veyron",
+                "Bolide",
+
+        };
+
+        final String[] porscheModels = new String[]{
+                "Cayenne",
+                "Macan",
+                "Taycan",
+                "Panamera",
+
+        };
+
+        final String[] bentleyModels = new String[]{
+                "Continental",
+                "Flying Spur",
+                "Bentayga",
+
+        };
+
+        final String[] ferrariModels = new String[]{
+                "Roma",
+                "Portofino",
+                "California",
+                "LaFerrari",
+
+        };
+
+        final String[] mercedesModels = new String[]{
+                "Classe A",
+                "Classe C",
+                "Classe E",
+                "Classe G",
+
+        };
+
+        ArrayAdapter<String> adapterCarBrand = new ArrayAdapter<>(
+                SignupActivity.this,
+                R.layout.dropdown_item,
+                carBrands
+        );
+        autoCompleteTextViewCarBrand.setAdapter(adapterCarBrand);
+
+        ArrayAdapter<String> adapterCarModel = new ArrayAdapter<>(
+                SignupActivity.this,
+                R.layout.dropdown_item,
+                carModels
+        );
+        autoCompleteTextViewCarModel.setAdapter(adapterCarModel);
+
+        autoCompleteTextViewCarBrand.setOnItemClickListener((parent, view, position, id) -> {
+            brand = autoCompleteTextViewCarBrand.getText().toString();
+            if (autoCompleteTextViewCarBrand.getText().toString().equals("Lamborghini")) {
+                ArrayAdapter<String> adapterLamborghini = new ArrayAdapter<>(
+                        SignupActivity.this,
+                        R.layout.dropdown_item,
+                        lamborghiniModels
+                );
+                autoCompleteTextViewCarModel.setAdapter(adapterLamborghini);
+            } else if (brand.equals("Bugatti")) {
+                ArrayAdapter<String> adapterBugatti = new ArrayAdapter<>(
+                        SignupActivity.this,
+                        R.layout.dropdown_item,
+                        bugattiModels
+                );
+                autoCompleteTextViewCarModel.setAdapter(adapterBugatti);
+            } else if (brand.equals("Porsche")) {
+                ArrayAdapter<String> adapterPorsche = new ArrayAdapter<>(
+                        SignupActivity.this,
+                        R.layout.dropdown_item,
+                        porscheModels
+                );
+                autoCompleteTextViewCarModel.setAdapter(adapterPorsche);
+            } else if (brand.equals("Ferrari")) {
+                ArrayAdapter<String> adapterFerrari = new ArrayAdapter<>(
+                        SignupActivity.this,
+                        R.layout.dropdown_item,
+                        ferrariModels
+                );
+                autoCompleteTextViewCarModel.setAdapter(adapterFerrari);
+            } else if (brand.equals("Bentley")) {
+                ArrayAdapter<String> adapterBentley = new ArrayAdapter<>(
+                        SignupActivity.this,
+                        R.layout.dropdown_item,
+                        bentleyModels
+                );
+                autoCompleteTextViewCarModel.setAdapter(adapterBentley);
+            } else if (brand.equals("Mercedes")){
+                ArrayAdapter<String> adapterMercedes = new ArrayAdapter<>(
+                        SignupActivity.this,
+                        R.layout.dropdown_item,
+                        mercedesModels
+                );
+                autoCompleteTextViewCarModel.setAdapter(adapterMercedes);
+            }
+
+
+        });
     }
 
     @Override
@@ -57,7 +190,9 @@ public class SignupActivity extends AppCompatActivity {
         // Sezione per il settaggio del pulsante signupButton, con controlli sugli elementi username, email e password
         signupButton.setOnClickListener(v -> {
 
-            if (Objects.requireNonNull(inputUsername.getEditText()).getText().toString().trim().equals("") || Objects.requireNonNull(inputEmail.getEditText()).getText().toString().trim().equals("") || Objects.requireNonNull(inputPassword.getEditText()).getText().toString().trim().equals("")) {
+            if (Objects.requireNonNull(inputUsername.getEditText()).getText().toString().trim().equals("")
+                    || Objects.requireNonNull(inputEmail.getEditText()).getText().toString().trim().equals("")
+                    || Objects.requireNonNull(inputPassword.getEditText()).getText().toString().trim().equals("")) {
                 Toast.makeText(getApplicationContext(), "Username, Email and Password cannot be empty", Toast.LENGTH_SHORT).show();
             }
             else if (!isValidEmail(inputEmail.getEditText().getText().toString().trim())) {
@@ -72,6 +207,11 @@ public class SignupActivity extends AppCompatActivity {
             else if (!Objects.requireNonNull(confirmPassword.getEditText()).getText().toString().equals(inputPassword.getEditText().getText().toString())) {
                 Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
             }
+            else if (Objects.requireNonNull(autoCompleteTextViewCarBrand).getText().toString().trim().equals("")
+                    || Objects.requireNonNull(autoCompleteTextViewCarModel).getText().toString().trim().equals("")
+                    || Objects.requireNonNull(autoCompleteTextViewCarModel).getText().toString().trim().equals("Choose a car brand first")) {
+                Toast.makeText(getApplicationContext(), "Car Brand or Model cannot be empty", Toast.LENGTH_SHORT).show();
+            }
             else {
 
                 fAuth.createUserWithEmailAndPassword(inputEmail.getEditText().getText().toString().trim(),inputPassword.getEditText().getText().toString().trim()).addOnCompleteListener((task) -> {
@@ -83,6 +223,8 @@ public class SignupActivity extends AppCompatActivity {
                         user.put("name", inputUsername.getEditText().getText().toString().trim());
                         user.put("email", inputEmail.getEditText().getText().toString().trim());
                         user.put("password", inputPassword.getEditText().getText().toString().trim());
+                        user.put("carBrand", autoCompleteTextViewCarBrand.getText().toString().trim());
+                        user.put("carModel", autoCompleteTextViewCarModel.getText().toString().trim());
                         documentReference.set(user).addOnSuccessListener(aVoid -> Log.d("TAG", "onSuccess: User profile is created for " + userID));
                         openLoginActivity();
                     }else{
